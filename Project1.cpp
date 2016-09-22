57a4b25f1566ca90f5d711d10eb25df32a932d18
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2016-09-21 19:44:33 -0700

    Xcode initialization.

---
+++ Project1.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Structures and Algorithms Projects
+//
+//  Created by Xavier Martinez on 9/21/16.
+//  Copyright © 2016 Xavier Martinez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
