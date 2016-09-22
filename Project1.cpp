f8b1817bda6ed5c08131080df38c0ccf676d1cf3
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2016-09-21 20:38:29 -0700

    Added completed task 1 & 2.

--- Project1.cpp
+++ Project1.cpp
@@ -1,5 +1,5 @@
 //
-//  main.cpp
+//  Project1.cpp
 //  Data Structures and Algorithms Projects
 //
 //  Created by Xavier Martinez on 9/21/16.
@@ -7,9 +7,50 @@
 //
 
 #include <iostream>
+using namespace std;
+
+int Equ1(int n){
+    if (n == 1) {
+        return 1;
+    }
+    else if (n == 2){
+        return 1;
+    }
+    else return (Equ1(n-1) + Equ1(n-2));
+}
+
+int Equ1_withoutRecusrion(int n){
+    cout << "n = " << n << endl;
+    cout << "f(1) = " << 1 << endl;
+    cout << "f(2) = " << 1 << endl;
+    int sum = 2;
+    int table[n];
+    table[0] = 1;
+    table[1] = 1;
+    for (int i = 2; i < n; i++) {
+        table[i] = table[i - 1] + table[i - 2];
+        cout << "f(" << i+1 << ") = " << table[i] << endl;
+        sum += table[i];
+    }
+    cout << "S(n) = " << sum << endl;
+    return sum;
+}
+
+
+int recursiveSum(int n){
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += Equ1(i);
+    }
+    return sum;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    /* Equ1: f(1) = 1
+             f(2) = 1
+             f(n) = f(n-1)+f(n-2)
+       Sum:  S(n) = f(0)+f(1)+...+f(n)
+     */
+    Equ1_withoutRecusrion(4);
     return 0;
 }
