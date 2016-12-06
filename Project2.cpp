40b2a749d7095eede715960f7e2e68c27cd1d824
Xavier Martinez <xavier@sorin11.campus.ad.csulb.edu>
2016-12-05 19:49:14 -0800

    major edits

--- Project2.cpp
+++ Project2.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <iomanip>
 #include <math.h>
 #include <ctime>
 #include <fstream>
@@ -15,221 +16,298 @@
 using namespace std;
 
 // Ensure minimum size of key is 25 by padding
-void smallKey(string key, char * newKey){
-    for (int i = 0; i < key.size(); i ++) {
+void smallKey(string key, char *newKey)
+{
+    for (int i = 0; i < key.size(); i++)
+    {
         newKey[i] = key[i];
     }
-    for (int i = key.size(); i < 25; i ++) {
-        newKey [i] = (char) 32;
+    for (int i = key.size(); i < 25; i++)
+    {
+        newKey[i] = (char)32;
     }
 }
 
 // Copy key into char array
-void largeKey(string key, char * newKey){
-    for (int i = 0; i < key.size(); i ++) {
+void largeKey(string key, char *newKey)
+{
+    for (int i = 0; i < key.size(); i++)
+    {
         newKey[i] = key[i];
     }
 }
 
 // Division Method
-unsigned long long hx(char * key, int size){
-    unsigned long long hash;
+unsigned long long int hx(char *key, int size, int tableSize)
+{
+    unsigned long long int hash = 0;
     cout << "\"";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         cout << key[i];
-        hash += (unsigned long long) key[i] * (unsigned long long) pow((double) 37, (double) i);
+        hash += (unsigned long long int)key[i] * (unsigned long long int)powl((long double)37, (long double)i);
     }
     cout << "\" hashes to: ";
-    hash = hash % 301;
+    hash = hash % (unsigned long long int)tableSize;
     cout << hash << endl;
     return hash;
 }
 
-// Multipliation Method
-unsigned long long hy(char * key, int size){
-    unsigned long long hash;
+// Multiplication Method
+unsigned long long hy(char *key, int size, int tableSize)
+{
+    unsigned long long hash = 0;
     cout << "\"";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         cout << key[i];
-        hash += (unsigned long long) key[i] * (unsigned long long) pow((double) 37, (double) i);
+        hash += (unsigned long long)key[i] * (unsigned long long)powl((long double)37, (long double)i);
     }
-    long double k = (long double)hash * (long double)0.618033989;
+    long double k = hash * ((long double)0.618033989);
     long double ro = floorl(k);
-    long double sub = k - ro;
+    long double sub = (long double)k - (long double)ro;
+    long double mul = ((long double)tableSize) * sub;
     cout << "\" hashes to: ";
-    hash = floorl(((long double)301.0) * sub);
+    hash = floorl(mul);
     cout << hash << endl;
     return hash;
 }
 
 // Measure the number of collisions
-int collisions(unsigned long long * values, int size){
+int collisions(unsigned long long *keyues, int size)
+{
     set<unsigned long long> collisions;
-    set<unsigned long long> collisionValues;
-    for (int i = 0; i < size; i++) {
-        for (int j = i; j < size; j++) {
-            if (i == j) {
+    set<unsigned long long> collisionkeyues;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i; j < size; j++)
+        {
+            if (i == j)
+            {
                 continue;
             }
-            else{
-                if (values[i] == values[j]) {
+            else
+            {
+                if (keyues[i] == keyues[j])
+                {
                     collisions.insert(i);
-                    collisionValues.insert(values[i]);
+                    collisionkeyues.insert(keyues[i]);
                     break;
                 }
             }
         }
     }
-    
-// Debug Code
-//    for (set<unsigned long long>::iterator it=collisionValues.begin(); it!=collisionValues.end(); ++it)
-//        cout << ' ' << *it;
-//    std::cout<<"\n";
-    
+
+    // Debug Code
+    //    for (set<unsigned long long>::iterator it=collisionkeyues.begin(); it!=collisionkeyues.end(); ++it)
+    //        cout << ' ' << *it;
+    //    std::cout<<"\n";
+
     return collisions.size();
 }
 
-struct node{
-    string val;
+// Node structure
+struct node
+{
+    string key;
+    string value;
     int next;
     int prev;
 };
 
-void linearProbing(node * table, int hash, int prev, string key){
+void linearProbing(node *table, int hash, int prev, string key, string value)
+{
     bool placed = false;
-    int probe = (hash + 1) % 301;
-    while(!placed){
-        if (table[probe].val == "/") {
-            table[probe].val = key;
-            table[hash].next = probe;
+    int probe = (hash + 1) % 51;
+    while (!placed)
+    {
+        if (table[probe].key == "/")
+        {
+            table[probe].key = key;
+            table[probe].value = value;
+            table[prev].next = probe;
             table[probe].prev = prev;
             placed = true;
         }
-        else{
-            probe = (probe + 1) % 301;
+        else
+        {
+            probe = (probe + 1) % 51;
         }
     }
 }
 
-void insert(node * table, unsigned long long hash, string key){
-    if (table[hash].val == "/") {
-        table[hash].val = key;
+void insert(node *table, int hash, string key, string value)
+{
+    if (table[hash].key == "/")
+    {
+        table[hash].key = key;
+        table[hash].value = value;
     }
-    else{
-        if (table[hash].next == -1) {
-            linearProbing(table, hash, hash, key);
+    else
+    {
+        if (table[hash].next == -1)
+        {
+            linearProbing(table, hash, hash, key, value);
         }
-        else {
+        else
+        {
             int current = table[hash].next;
             bool found = false;
-            while (!found) {
-                if (table[current].next == -1) {
+            while (!found)
+            {
+                if (table[current].next == -1)
+                {
                     found = true;
-                    linearProbing(table, hash, current, key);
+                    linearProbing(table, hash, current, key, value);
                 }
-                else{
+                else
+                {
                     current = table[current].next;
                 }
             }
         }
-        
     }
 }
 
-void displayTable(node * table, int size){
-    for (int i = 0; i < size; i++){
-        cout << i << "\t|" << table[i].val << "\t|" << table[i].next << "\t|" << table[i].prev << endl;
+void search(node * table, string key){
+    if (key.size() > 25)
+            {
+                char keyArr[key.size()];
+                largeKey(key, keyArr);
+                unsigned long long hashx = hx(keyArr, key.size(), 301);
+                if (table[hashx] == key)
+                {
+                    cout << table[hashx].value << endl;
+                }
+            }
+            else
+            {
+                char keyArr[25];
+                smallKey(key, keyArr);
+                unsigned long long hashx = hx(keyArr, 25, 301);
+                unsigned long long hashy = hy(keyArr, 25, 301);
+                divisionMethodkeyues[pos] = hashx;
+                multiplicationMethodkeyues[pos] = hashy;
+            }
+}
+
+void displayTable(node *table, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << i << "\t|" << setw(32) << table[i].key << "|" << setw(60) << table[i].value.substr(0, 55) << "\t|" << table[i].next << "\t|" << table[i].prev << endl;
     }
 }
 
+int main()
+{
+    cout.precision(100);
+    string titles = "/home/xavier/git-repositories/Data-Structures-and-Algorithms-Repository/WikiTitles.txt";
+    string leaks = "/home/xavier/git-repositories/Data-Structures-and-Algorithms-Repository/wikileaks.txt";
+    string wd = "/home/xavier/git-repositories/Data-Structures-and-Algorithms-Repository/";
+    // Implementing the hash data structure
+    node table[51];
+    for (int i = 0; i < 51; i++)
+    {
+        table[i].key = "/";
+        table[i].value = "/";
+        table[i].next = -1;
+        table[i].prev = -1;
+    }
 
-int main(){
-    cout.precision(40);
-    
     // Declare Variables to be used in performance test
     string key;
-    unsigned long long divisionMethodValues[100];
-    unsigned long long multiplicationMethodValues[100];
-    
+    unsigned long long divisionMethodkeyues[100];
+    unsigned long long multiplicationMethodkeyues[100];
+
     // Open the 100 Wiki Titles for reading.
     ifstream wikiTitles;
-    wikiTitles.open("/Users/xaviermartinez/Documents/CECS 328 Projects/Data Structures and Algorithms Projects/Data Structures and Algorithms Projects/Data-Structures-and-Algorithms-Repository/WikiTitles.txt");
-    if(!wikiTitles)
+    wikiTitles.open(titles);
+    if (!wikiTitles)
     {
         cout << "ERROR: UNABLE TO OPEN INPUT DATA!" << endl;
         exit(EXIT_FAILURE);
     }
-    else {
-        // Test the Division Method
-        cout << "## DIVISION METHOD ##" << endl;
+    else
+    {
         int pos = 0;
-        while(getline(wikiTitles, key)){
+        while (getline(wikiTitles, key))
+        {
             // Determine whther or not to add padding
-            if (key.size() > 25) {
-                char keyArr [key.size()];
+            if (key.size() > 25)
+            {
+                char keyArr[key.size()];
                 largeKey(key, keyArr);
-                divisionMethodValues[pos] = hx(keyArr, key.size());
+                unsigned long long hashx = hx(keyArr, key.size(), 301);
+                unsigned long long hashy = hy(keyArr, key.size(), 301);
+                divisionMethodkeyues[pos] = hashx;
+                multiplicationMethodkeyues[pos] = hashy;
             }
-            else {
-                char keyArr [25];
+            else
+            {
+                char keyArr[25];
                 smallKey(key, keyArr);
-                divisionMethodValues[pos] = hx(keyArr, 25);
+                unsigned long long hashx = hx(keyArr, 25, 301);
+                unsigned long long hashy = hy(keyArr, 25, 301);
+                divisionMethodkeyues[pos] = hashx;
+                multiplicationMethodkeyues[pos] = hashy;
             }
             pos++;
         }
-        cout << "The division method exhibited " << collisions(divisionMethodValues, 100) << " collisions!" << endl;
-        
-        // Clear reader to start over from the beginning of the file
-        wikiTitles.clear();
-        wikiTitles.seekg(0, ios::beg);
-        
-        // Test the multiplication method
-        cout << "## MULTIPLICATION METHOD ##" << endl;
-        pos = 0;
-        while(getline(wikiTitles, key)){
-            // Determine whther or not to add padding
-            if (key.size() > 25) {
-                char keyArr [key.size()];
-                largeKey(key, keyArr);
-                multiplicationMethodValues[pos] = hy(keyArr, key.size());
+        cout << "The division method exhibited " << collisions(divisionMethodkeyues, 100) << " collisions!" << endl;
+        cout << "The multiplication method exhibited " << collisions(multiplicationMethodkeyues, 100) << " collisions!" << endl;
+        cout << endl;
+    }
+    wikiTitles.close();
+
+    // Implementation of the hash data structure
+    cout << "## Implementation of hash data structure ##" << endl;
+    string leak;
+    ifstream wikileaks;
+    wikileaks.open(leaks);
+    if (!wikileaks)
+    {
+        cout << "ERROR: UNABLE TO OPEN INPUT DATA!" << endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        while (getline(wikileaks, leak))
+        {
+            string tmp = leak;
+            leak.append(".txt");
+            leak = wd + leak;
+            string value;
+            ifstream summary;
+            summary.open(leak);
+            if (!summary)
+            {
+                cout << "ERROR: UNABLE TO OPEN SUMMARY DATA!" << endl;
+                exit(EXIT_FAILURE);
             }
-            else {
-                char keyArr [25];
-                smallKey(key, keyArr);
-                multiplicationMethodValues[pos] = hy(keyArr, 25);
+            else
+            {
+                getline(summary, value);
             }
-            pos++;
-        }
-        cout << "The multiplication method exhibited " << collisions(multiplicationMethodValues, 100) << " collisions!" << endl;
-    }
-    
-    // Begin implementing the hash data structure
-    node table [100];
-    for (int i = 0; i < 100; i ++){
-        table[i].val = "/";
-        table[i].next = -1;
-        table[i].prev = -1;
-    }
-    
-    // Clear reader to start over from the beginning of the file
-    wikiTitles.clear();
-    wikiTitles.seekg(0, ios::beg);
-    
-    while(getline(wikiTitles, key)){
-        // Determine whther or not to add padding
-        if (key.size() > 25) {
-            char keyArr [key.size()];
-            largeKey(key, keyArr);
-            unsigned long long hash = hx(keyArr, key.size());
-            insert(table, hash, key);
-        }
-        else {
-            char keyArr [25];
-            smallKey(key, keyArr);
-            unsigned long long hash = hx(keyArr, 25);
-            insert(table, hash, key);
+            // Determine whether or not to add padding
+            if (tmp.size() > 25)
+            {
+                char keyArr[tmp.size()];
+                largeKey(tmp, keyArr);
+                unsigned long long hashx = hx(keyArr, tmp.size(), 51);
+                insert(table, hashx, tmp, value);
+            }
+            else
+            {
+                char keyArr[25];
+                smallKey(tmp, keyArr);
+                unsigned long long hashx = hx(keyArr, 25, 51);
+                insert(table, hashx, tmp, value);
+            }
+            summary.close();
         }
     }
-    displayTable(table, 100);
+    displayTable(table, 51);
+    wikileaks.close();
     return 0;
 }
